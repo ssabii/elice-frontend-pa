525ea7cab98a94a5980518dcea665a83e7faa870
ssabii <fhy1930@gmail.com>
2022-11-11 23:50:23 +0900

    feat: CoursePage 컴포넌트 구현

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+
 import Layout from "layouts/Layout";
+import CoursePage from "pages/course/CoursePage";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<div>Main</div>} />
+        <Route index element={<CoursePage />} />
       </Route>
     </Routes>
   );
