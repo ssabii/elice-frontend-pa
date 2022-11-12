8abcc67855d1ef306cfb41ecddd4ef768ac70473
ssabii <fhy1930@gmail.com>
2022-11-12 22:51:41 +0900

    feat: CourseList store 연결

--- src/pages/course/CourseList.tsx
+++ src/pages/course/CourseList.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Pagination from "components/Pagination";
+import useCourse from "hooks/course/useCourse";
 import styled from "styled-components";
 
 import CourseCard from "./CourseCard";
 
 const CourseList = () => {
+  const { page, pagination, setPage } = useCourse();
+  const { count, totalCount } = page;
+  const { current, last } = pagination;
+
+  const handlePageChange = useCallback(
+    (value: number) => {
+      setPage({
+        ...page,
+        offset: (value - 1) * count,
+      });
+    },
+    [page, count, setPage],
+  );
+
   return (
     <Container>
       <CountWrapper>
-        <Count>전체 123개</Count>
+        <Count>전체 {totalCount}개</Count>
       </CountWrapper>
       <CourseCardContainer>
         <CourseCard
@@ -19,9 +34,15 @@ const CourseList = () => {
           src="https://cdn-api.elice.io/api/file/09a0fa8235df42a5976316b97b6dcd56/%E1%84%8F%E1%85%A2%E1%84%80%E1%85%B3%E1%86%AF_R_%E1%84%83%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%90%E1%85%A5%E1%84%87%E1%85%AE%E1%86%AB%E1%84%89%E1%85%A5%E1%86%A8%402x.png?se=2022-11-24T00%3A15%3A00Z&sp=rt&sv=2020-10-02&sr=b&sig=kphFGTdB3SFYx0F3UP5d1iwpy9AQKEs255sYWPM5D8A%3D"
         />
       </CourseCardContainer>
-      <PaginationWrapper>
-        <Pagination current={7} total={20} />
-      </PaginationWrapper>
+      {totalCount > 0 && (
+        <PaginationWrapper>
+          <Pagination
+            current={current}
+            last={last}
+            onPageChange={handlePageChange}
+          />
+        </PaginationWrapper>
+      )}
     </Container>
   );
 };
