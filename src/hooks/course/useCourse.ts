550be8bafca31c41bf79614b0f4d1719d5895523
ssabii <fhy1930@gmail.com>
2022-11-12 22:28:40 +0900

    feat: course hooks selector 추가

--- src/hooks/course/useCourse.ts
+++ src/hooks/course/useCourse.ts
@@ -1,15 +1,22 @@
-import { useRecoilState } from "recoil";
-import { keywordState, pageState, priceFilterState } from "store/course";
+import { useRecoilState, useRecoilValue } from "recoil";
+import {
+  keywordState,
+  pageState,
+  paginationState,
+  priceFilterState,
+} from "store/course";
 
 const useCourse = () => {
   const [keyword, setKeyword] = useRecoilState(keywordState);
   const [priceFilter, setPriceFilter] = useRecoilState(priceFilterState);
   const [page, setPage] = useRecoilState(pageState);
+  const pagination = useRecoilValue(paginationState);
 
   return {
     keyword,
     priceFilter,
     page,
+    pagination,
     setKeyword,
     setPriceFilter,
     setPage,
