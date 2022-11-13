4b57570578729528745f51e97a217f8420c00207
ssabii <fhy1930@gmail.com>
2022-11-13 22:22:10 +0900

    feat: CourseList 데이터 연동

--- src/pages/course/CourseList.tsx
+++ src/pages/course/CourseList.tsx
@@ -1,6 +1,8 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 import Pagination from "components/Pagination";
+import useCourseFilter from "hooks/course/useCourseFilter";
+import useCourses from "queries/course/useCourses";
 import styled from "styled-components";
 
 import CourseCard from "./CourseCard";
@@ -9,32 +11,64 @@ const DEFAULT_COUNT = 20;
 
 const CourseList = () => {
   const [offset, setOffset] = useState(0);
+  const { keyword, price } = useCourseFilter();
+  const { data, isLoading } = useCourses({
+    params: { keyword, price },
+    offset,
+    count: DEFAULT_COUNT,
+  });
 
-  const handlePageChange = useCallback((value: number) => {
-    const newOffset = (value - 1) * DEFAULT_COUNT;
-    setOffset(newOffset);
-  }, []);
+  const current = useMemo(
+    () => Math.floor(offset / DEFAULT_COUNT) + 1,
+    [offset],
+  );
+  const totalCount = useMemo(() => data?.totalCount ?? 0, [data]);
+  const last = useMemo(
+    () => Math.ceil(totalCount / DEFAULT_COUNT),
+    [totalCount],
+  );
+
+  const handlePageChange = useCallback(
+    (value: number) => {
+      const newOffset = (value - 1) * DEFAULT_COUNT;
+      setOffset(newOffset);
+    },
+    [setOffset],
+  );
+
+  useEffect(() => {
+    setOffset(0);
+  }, [keyword, price]);
 
   return (
     <Container>
       <CountWrapper>
-        <Count>전체 123개</Count>
+        <Count>전체 {totalCount}개</Count>
       </CountWrapper>
       <CourseCardContainer>
-        <CourseCard
-          label="무료"
-          title="캐글 문제로 배우는 R 데이터 분석"
-          description="실전 캐글 대회 문제로 익히는 R 데이터 분석!"
-          src="https://cdn-api.elice.io/api/file/09a0fa8235df42a5976316b97b6dcd56/%E1%84%8F%E1%85%A2%E1%84%80%E1%85%B3%E1%86%AF_R_%E1%84%83%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%90%E1%85%A5%E1%84%87%E1%85%AE%E1%86%AB%E1%84%89%E1%85%A5%E1%86%A8%402x.png?se=2022-11-24T00%3A15%3A00Z&sp=rt&sv=2020-10-02&sr=b&sig=kphFGTdB3SFYx0F3UP5d1iwpy9AQKEs255sYWPM5D8A%3D"
-        />
+        {isLoading
+          ? null
+          : data?.courses.map(({ id, label, title, description, src }) => {
+              return (
+                <CourseCard
+                  key={id}
+                  label={label}
+                  title={title}
+                  description={description}
+                  src={src}
+                />
+              );
+            })}
       </CourseCardContainer>
-      <PaginationWrapper>
-        <Pagination
-          current={Math.floor(offset / DEFAULT_COUNT) + 1}
-          last={10}
-          onPageChange={handlePageChange}
-        />
-      </PaginationWrapper>
+      {totalCount > 0 && (
+        <PaginationWrapper>
+          <Pagination
+            current={current}
+            last={last}
+            onPageChange={handlePageChange}
+          />
+        </PaginationWrapper>
+      )}
     </Container>
   );
 };
