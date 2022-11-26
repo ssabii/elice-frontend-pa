d007cacd6aea4bb508e13eb221c95d6072535234
ssabii <fhy1930@gmail.com>
2022-11-26 23:34:42 +0900

    feat: useDobounce hooks 구현

---
+++ src/hooks/course/useDebounce.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useDebounce = (value: string, delay: number) => {
+  const [input, setInput] = useState(value);
+  
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setInput(value);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return input;
+};
+
+export default useDebounce;
